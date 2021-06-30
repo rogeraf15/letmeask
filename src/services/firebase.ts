ab2bf4811fdf7a4e425b23012f97b6fcc140dc25
Roger Fernandes <roger.afernandes@hotmail.com>
2021-06-30 09:20:39 -0300

    add env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDEp5AaJWitmVIrRK4_DovxmembMSg-b0o",
-  authDomain: "letmeask-e2c1d.firebaseapp.com",
-  databaseURL: "https://letmeask-e2c1d-default-rtdb.firebaseio.com",
-  projectId: "letmeask-e2c1d",
-  storageBucket: "letmeask-e2c1d.appspot.com",
-  messagingSenderId: "501760822665",
-  appId: "1:501760822665:web:cb2cebed314b7929a4a558",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
